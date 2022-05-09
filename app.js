69072f996a5c4652b7b3a99888489a07c5fb4c20
TeamAbella060420 <jabella@rscm.com.sa>
2022-05-09 15:34:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-$(document).foundation();
-
-// ============
-// TO TOP BUTTON
-// ============
-// Display the "to top" button only when the user has scrolled down the page 300px
-window.onscroll = function() {
-    displayToTopBtn();
-};
-
-function displayToTopBtn() {
-    const toTheTopBtn = document.getElementById('top');
-    // Check scroll position
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-        toTheTopBtn.style.display = 'block';
-    } else {
-        toTheTopBtn.style.display = 'none';
-    }
-}
-
 // ============
 // HERO
 // ============
