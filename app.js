2f92dafc63d4db60102ee7000c78fd34ae3f9b4c
TeamAbella060420 <jabella@rscm.com.sa>
2022-06-06 11:14:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-// ============
-// HERO
-// ============
-// Typed library by https://mattboldt.com/demos/typed-js/
-const typed = new Typed('#typed', {
-    strings: ["JavaScript...", "HTML5...", "CSS3...", "Front-End Developer."],
-    typeSpeed: 100,
-    smartBackspace: true,
-    startDelay: 200});
+// // ============
+// // HERO
+// // ============
+// // Typed library by https://mattboldt.com/demos/typed-js/
+// const typed = new Typed('#typed', {
+//     strings: ["JavaScript...", "HTML5...", "CSS3...", "Front-End Developer."],
+//     typeSpeed: 100,
+//     smartBackspace: true,
+//     startDelay: 200});
+
+const data = { 
+    name: 'Anna',
+     age: 18,
+     name: 'Alex', 
+    age: 21 ,
+    name: 'Meghan', 
+    age: 12 
+    };
